1dca65ad944db07292e077266478fce69a2c1e1c
Jeff <jhwe92@gmail.com>
2017-02-23 13:02:55 -0800

    added registration page and functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
 
   def create
   end
+
+  def register
+    user = User.create(name: params['name'], email: params['email'], password: params['pw'], password_confirmation: params['pw_confirm'])
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to ("/view/#{user.id}")
+    else
+      flash[:errors] = user.errors.full_messages.to_sentence
+      redirect_to ("/")
+  end
 end
