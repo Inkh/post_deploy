585a4d530b19563e4f014b57acd8318dc185be10
Jeff <jhwe92@gmail.com>
2017-02-23 13:10:47 -0800

    added log in field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,30 @@ class UsersController < ApplicationController
   end
 
   def view
+      # @user = User.find(session[:user_id])
+      @user = User.find(params[:id])
   end
 
   def create
   end
 
+  def log
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      puts user.id
+      session[:user_id] = user.id
+      redirect_to ("/view/#{user.id}")
+    else
+      flash[:log_error] = "Something was wrong with log in credentials, please try again"
+      redirect_to ('/')
+    end
+  end
+
+  def out
+
+  end
+  
   def register
     user = User.create(name: params['name'], email: params['email'], password: params['pw'], password_confirmation: params['pw_confirm'])
     if user.valid?
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'users#index'
 
-  get 'users/view'
+  get 'view/:id'
 
   post 'register' => 'users#register'
+
+
   get 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
